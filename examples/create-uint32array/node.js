d2a36aec5dae24b9a4e95e4f055109a73d3b6bbc
RafaelGSS <rafael.nunu@hotmail.com>
2023-12-19 17:17:43 -0300

    fix: typo

--- examples/create-uint32array/node.js
+++ examples/create-uint32array/node.js
@@ -6,10 +6,4 @@ suite
   .add(`new Uint32Array(1024)`, function () {
     return new Uint32Array(1024);
   })
-  .add(`new Uint32Array(1024 * 1024)`, function () {
-    return new Uint32Array(1024 * 1024);
-  })
-  .add(`new Uint32Array(1024 * 1024 * 10)`, function () {
-    return new Uint32Array(1024 * 1024 * 10);
-  })
   .run();

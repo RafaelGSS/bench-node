ddf6dc7b4e7a695f6bff5766788b4b0d5beec527
RafaelGSS <rafael.nunu@hotmail.com>
2024-10-14 21:47:15 -0300

    test: add test case for copy function
    Refs: https://github.com/RafaelGSS/bench-node/issues/12

---
+++ test/env.js
@@ -0,0 +1,32 @@
+const { describe, it, before } = require('node:test');
+const assert = require('node:assert');
+const copyBench = require('./fixtures/copy');
+
+describe('Same benchmark function', () => {
+  let results;
+
+  before(async () => {
+    results = await copyBench.run();
+  });
+
+  it('must have a similar benchmark result', () => {
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results.length; j++) {
+        if (i !== j) {
+          const opsSec1 = results[i].opsSec;
+          const opsSec2 = results[j].opsSec;
+
+          // Calculate the percentage difference
+          const difference = Math.abs(opsSec1 - opsSec2);
+          const percentageDifference = (difference / Math.min(opsSec1, opsSec2)) * 100;
+
+          // Check if the percentage difference is less than or equal to 10%
+          assert.ok(
+            percentageDifference <= 10,
+            `${opsSec1} too different from ${opsSec2} - ${results[i].name}`
+          );
+        }
+      }
+    }
+  });
+});

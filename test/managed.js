d30c874d284e84a2aef60316a35ae50c8ad7e8c9
RafaelGSS <rafael.nunu@hotmail.com>
2024-12-30 14:50:45 -0300

    test: add managed basic tests

--- test/managed.js
+++ test/managed.js
@@ -1,3 +1,33 @@
-const { todo } = require("node:test");
+const { describe, it } = require("node:test");
+const assert = require("node:assert");
+const { Suite } = require("../lib/index");
 
-todo("managed benchmarks", async () => {});
+describe("managed benchmarks", async () => {
+	it("should throw when timer.start isn't called", () => {
+		const suite = new Suite({ reporter: () => {} });
+		assert.rejects(
+			async () => {
+				suite.add("managed", (timer) => {});
+				await suite.run();
+			},
+			{
+				message: "You forgot to call .start()",
+			},
+		);
+	});
+
+	it("should throw when timer.end isn't called", () => {
+		const suite = new Suite({ reporter: () => {} });
+		assert.rejects(
+			async () => {
+				suite.add("managed", (timer) => {
+					timer.start();
+				});
+				await suite.run();
+			},
+			{
+				message: "You forgot to call .end(count)",
+			},
+		);
+	});
+});

b207a79543ca2706ef1af2f74abe2118c5668ff9
RafaelGSS <rafael.nunu@hotmail.com>
2025-07-01 18:39:02 -0300

    feat: add pretty-reporter and shorthand pretty: true
    Signed-off-by: RafaelGSS <rafael.nunu@hotmail.com>

--- examples/pretty-report/node.js
+++ examples/pretty-report/node.js
@@ -1,19 +1,24 @@
-const { Suite, csvReport } = require('../../lib');
+const { Suite, prettyReport } = require('../../lib');
 const assert = require('node:assert');
 
 const suite = new Suite({
-  reporter: csvReport,
+  reporter: prettyReport,
 });
 
 suite
-  .add('single with matcher', function () {
+  .add('my-group/my-benchmark', () => {
     const pattern = /[123]/g
     const replacements = { 1: 'a', 2: 'b', 3: 'c' }
-    const subject = '123123123123123123123123123123123123123123123123'
+    const subject = '12312312312312312312312'
     const r = subject.replace(pattern, m => replacements[m])
     assert.ok(r);
   })
-  .add('multiple replaces', function () {
+  .add('my-group/my-benchmark-2', function () {
+    const subject = '123123123123'
+    const r = subject.replace(/1/g, 'a').replace(/2/g, 'b').replace(/3/g, 'c')
+    assert.ok(r);
+  })
+  .add('second-group/baseline', function () {
     const subject = '123123123123123123123123123123123123123123123123'
     const r = subject.replace(/1/g, 'a').replace(/2/g, 'b').replace(/3/g, 'c')
     assert.ok(r);

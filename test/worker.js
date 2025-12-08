d30e4b78b8b7e815297bda4a49a380364278f694
Jason <jdmarshall@users.noreply.github.com>
2025-12-08 12:33:56 -0800

    chore: Code review revisions.
    Emulate the same optimizations from clock.js in worker-runner.js.
    
    This will only create an async function if the origin function is
    async, and will only trigger the Timer logic flow if the origin
    function contained the same.

--- test/worker.js
+++ test/worker.js
@@ -22,7 +22,10 @@ describe("Using worker_threads", () => {
 			.add("Import without node: prefix", () => {
 				return import("node:fs");
 			})
-			.add("async test", async (timer) => {
+			.add("async test", async () => {
+				return import("node:fs");
+			})
+			.add("async with timer", async (timer) => {
 				timer.start();
 				let i = 0;
 				while (i++ < timer.count) {
@@ -38,7 +41,7 @@ describe("Using worker_threads", () => {
 		mock.restoreAll();
 	});
 
-	it("should create a new Worker 3 times", () => {
-		assert.strictEqual(workerThreads.Worker.mock.calls.length, 3);
+	it("should create a new Worker 4 times", () => {
+		assert.strictEqual(workerThreads.Worker.mock.calls.length, 4);
 	});
 });
